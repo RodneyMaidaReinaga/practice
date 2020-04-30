171decce49f7c6862089a7ff7a13b2db5d22ed58
Rodney Maida Reinaga <rodmaidareinaga@gmail.com>
2020-04-30 10:31:34 -0400

    Added changes to spring project - Practice 1: - Points 1 and 2 done

--- src/main/java/com/jalasoft/practice/controller/HelloController.java
+++ src/main/java/com/jalasoft/practice/controller/HelloController.java
@@ -1,4 +1,22 @@
 package com.jalasoft.practice.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
+//    @PostMapping
+//    public String sayHello() {
+//        return "Hellow World!!!";
+//    }
+
+    @PostMapping
+    public String sayHello(@RequestParam(value="name") String name,
+                           @RequestParam(value="lastName") String lname) {
+        return "Hellow " + name + " " + lname;
+    }
 }
