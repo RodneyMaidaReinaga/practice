1fa8359c53b4e7d0982138bb4733aa42e40f89a7
Rodney Maida Reinaga <rodmaidareinaga@gmail.com>
2020-05-03 20:13:41 -0400

    Added copyright, documentaion and removed an extra line

--- src/main/java/com/jalasoft/practice/controller/HelloController.java
+++ src/main/java/com/jalasoft/practice/controller/HelloController.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
 package com.jalasoft.practice.controller;
 
 import org.springframework.util.StringUtils;
@@ -13,9 +21,32 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+/**
+ * Handles the Post request to the application, receive 4 parameters and either say hello, or copy a file or both
+ * depending if the parameters has values or not
+ *
+ * @author Rodney Maida
+ * @version 1.0
+ */
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+    /**
+     * Receive 4 parameters (1) a String parameter called name, (2) a String Parameter called lastName,
+     * if these two Parameters are empty, it prints pu "Hello Work", if any of those parameters or both
+     * parameters have a value prints out "Hello" + the value of those parameters
+     * (3) a String parameter called destinyPath which specify where the file received will be saved,
+     * if the destinyPath parameter is empty it saves the file by default at "inputFiles\", if it has a value
+     * verify that the path provided is a valid one and if it is saves the file there
+     * (4) a MultipartFile parameter called file, if a file is provided is saved to the location
+     * specified in the destinyPath parameter, if the file parameter is empty jsut ignore this and do the Hello
+     * part specified on the name and lastName parameters
+     *
+     * @author Rodney Maida
+     * @version 1.0
+     */
 
     @PostMapping
     public String practiceOne(@RequestParam(value = "name") String name,
@@ -30,7 +61,6 @@ public class HelloController {
         } else {
             return sayHello(name, lastName);
         }
-
     }
 
     private String sayHello(){
@@ -69,9 +99,6 @@ public class HelloController {
         if (lastChar == '\\') {
             return path;
         }
-
         return path + "\\";
-
     }
-
 }
